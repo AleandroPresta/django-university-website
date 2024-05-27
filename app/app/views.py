3dac3a9c01cde714508b9c33002c49e8ae2e23bc
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-05-27 12:30:53 +0200

    Loading whole site

---
+++ app/app/views.py
@@ -0,0 +1,17 @@
+# views.py
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def course(request):
+    return render(request, 'course.html')
